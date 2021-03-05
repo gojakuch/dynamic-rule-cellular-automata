4d5a5ec712f711b72665dfd817ea1b3780ea75f4
Atell <delta_atell@protonmail.com>
2021-03-05 16:47:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Dynamic Rule Cellular Automata
+The original idea of the experiment was to create a "second-order rule" for cellular automata which would modify the rule on each iteration. The "second-order rule" is represented with a string variable `order2_rule` on line five in the code. If you leave the string empty, you will get a regular elementary cellular automaton.
+Such automata are still completely deterministic, yet at times they have a bigger degree of variation and provide more space for dynamics to produce chaos and complexity. However, in many cases, especially when performing binary shifts on the rule, modified automata end up alternating full-black and full-white states after producing various branched patterns.
+## Second-order rule notation
+In this experiment, the are five basic operations that can be performed on the rule. These include:
+```
+> - shift bits in the rule to the right
+< - shift bits in the rule to the left
++ - add 1 to the rule number
+- - subtract 1 from the rule number
+| - mirror the binary representation of the rule
+```
+All of these can be combined in a sequence to form a string in `order2_rule`. For instance:
+```julia
+order2_rule = ">>+"
+```
+The above configuration means that on each iteration we will shift the binary representation of the rule by two and add one to what we have got. Of course, the second-order rule sequence of operations is not associative and `"+>"` is not the same as `">+"`, so the order matters.
+Note that `>` and `<` operations are circular, so:
+```
+01100111
+perform '>'
+10110011
+```
+## Libraries and usage
+All the code was originally implemented in Julia v1.4. I have used matplotlib for the graphics through PyPlot and PyCall (you can install those with no problem using Pkg). The code works both in Juno environment displaying graphics in the Plots section and from the terminal via the standard matplotlib view (see the last two lines of code).
+### Config
+All the configurations to select a specific automaton can be accessed in the first block of code.
+## License
+The code of the experiment is provided under the MIT license. Please, leave a credit to me (Atell Krasnopolski) and this repository, in case you use this software as a tool for your experiments/research or if you develop or mention any of my ideas presented here (specific implementation of dynamic rule cellular automata, bit shift on rules, etc).
---
+++ main.jl
@@ -0,0 +1,134 @@
+## Config (changes allowed)
+latlen = 2000      # lattice length
+iters = 500       # iterations
+start_rule = 30   # starting rule
+order2_rule = "+"  # the rule of changing the rule
+           #      - no changes (regular auomata)
+           # >    - shift one to the right
+           # >>   - shift two to the right
+           # <    - shift one to the left
+           # <<   - shift two to the left
+           # +    - next rule (increases Wolfram's number)
+           # -    - previous rule (decreases Wolfram's number)
+           # |    - flip the rule by bits (mirror)
+           # +>>  - combination (increase rule and move two rules to the right). + operation cometh first
+init = "."        # initial distribution of cells
+           # .  - dot
+           # r  - random
+           # i  - inverse
+           # :  - alternating black and white cells
+           # .i - combination (inverse dot)
+           # a  - get from init_arr
+init_arr = []     # initial distribution of cells (leave blank if init is not "a").
+                  # watch out, init_arr needs to be of length latlen
+
+
+## Some interesting configurations
+#=
+starting with dot:
+# 60 >>
+# 60 >>|<<
+# 60 >|<
+# 60 <|>
+# 60 >+<
+# 101
+# 101 >
+# 101 >>
+# 101 >|
+# 101 |
+random start:
+# 40 +
+# 40 >+<
+# 50 >|<
+# 60 >|<
+# 105 >>
+# 110 +<
+# 110 <
+# 110 +
+# 110 -
+# 110 --
+# 110 >+++<
+=#
+
+
+## Config pricessing (changes not recommended)
+# rule is a vector of integers (1s and 0s)
+rule = [Int(i)-48 for i::Char in Base.bin(Unsigned(start_rule), 8, false)]
+
+# a function to change the rule number by one
+function next_rule!(rulevec::Vector{Int}, step::Int)
+    num::Int = parse(Int, join(rulevec), base=2)
+    num += step
+    if num > 255
+        num -= 255
+    elseif num < 0
+        num = 256 + num
+    end
+    b::String = Base.bin(Unsigned(num), 8, false)
+    for i in 1:length(rulevec)
+        rulevec[i] = Int(b[i])-48
+    end
+    return nothing
+end
+
+# a mapping from characters to actions (order2 rules)
+char2rule = Dict([
+    '+' => r -> next_rule!(r, 1),
+    '-' => r -> next_rule!(r, -1),
+    '>' => r -> circshift!(r, copy(r), 1),
+    '<' => r -> circshift!(r, copy(r), -1),
+    '|' => r -> reverse!(r)
+])
+
+
+## Import and setup for visualization
+using PyCall
+import PyPlot; const plt = PyPlot
+rcParams = PyDict(plt.matplotlib."rcParams")
+rcParams["image.cmap"] = "binary"
+
+
+## Data init (first line)
+data = []
+if occursin("r", init)
+    push!(data, [rand((0, 1)) for i::Int in range(0, length=latlen)])
+elseif occursin(".", init)
+    push!(data, [((i==round(latlen/2)) ? 1 : 0) for i::Int in range(0, length=latlen)])
+elseif occursin("a", init)
+    data = [copy(init_arr)]
+end
+if occursin("i", init)
+    data = [[abs(i-1) for i::Int in data[1]]]
+end
+
+
+## The magic
+for iter::Int in 1:iters
+    new_line::Vector{Int} = []
+    for i::Int in 1:length(data[iter])
+        neigh::String = ""
+        for j::Int in i-1:i+1
+            aj::Unsigned = j # actual index to look at
+            if (j <= 0)
+                aj = length(data[iter])-1+j
+            elseif (j > length(data[iter]))
+                aj = j - length(data[iter])
+            end
+            neigh *= string(data[iter][aj])
+        end
+        push!(new_line, rule[8-parse(Int, neigh, base=2)])
+    end
+    push!(data, new_line)
+    # change the rule according to the order2_rule
+    for c::Char in order2_rule
+        char2rule[c](rule)
+    end
+end
+
+
+## Visualization
+fig, ax = plt.subplots(figsize=(80, 80))
+ax.matshow(data)
+ax.axis(false)
+display(plt.gcf()) # for Juno Plots window
+plt.show()         # for terminal usage
